0c67f17d9a5e350edbd51ca9624e698c6b12ac72
realsonofmary <realsonofmary@gmail.com>
2025-03-17 02:28:03 -0600

    meu primeiro projeto java

---
+++ src/main/java/org/yourcompany/yourproject/MEUPRIMEIROPROJETOJAVA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author sonofjesus
+ */
+public class MEUPRIMEIROPROJETOJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Boas vindas, time turma DIO");
+    }
+}
